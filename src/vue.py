af6b1b1767afad7c5a0546abb4b195a9c8463e85
Suraj <S_pun@outlook.com>
2024-02-03 23:07:23 +0000

    img downloader function

--- src/vue.py
+++ src/vue.py
@@ -6,3 +6,16 @@ from selenium.webdriver.chrome.service import Service # Allows you to start the 
 from selenium.webdriver.support.ui import WebDriverWait # Provides the ability to wait for a certain condition to occur before proceeding.
 from selenium.webdriver.support import expected_conditions as EC # Collection of predefined conditions to use with WebDriverWait.
 from selenium.common.exceptions import NoSuchElementException # Exception thrown when an element is not found.
+
+# Image Downloader
+
+def download_image(image_url, file_name):
+    # Check if the directory exists where the image will be saved
+    directory = os.path.dirname(file_name)
+    if not os.path.exists(directory):
+        os.makedirs(directory)  # Create the directory if it does not exist
+    
+    response = requests.get(image_url)
+    if response.status_code == 200:
+        with open(file_name, 'wb') as file:
+            file.write(response.content)
